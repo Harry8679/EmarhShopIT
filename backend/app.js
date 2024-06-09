fa8114b7bb2134563f4ca7000124dc66dcd2e13c
Harry MacCode <emarh.harry.code@gmail.com>
2024-06-09 13:00:46 +0200

    Installing & Setting up Nodemon

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({ path: 'backend/.env' });
 const app = express();
 
 const port = process.env.PORT || 6001;
 
 app.listen(port, () => {
-    console.log(`Server started on PORT ${port}`);
+    console.log(`Server started on PORT ${port} in ${process.env.NODE_ENV}`);
 });
