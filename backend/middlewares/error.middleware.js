335234cce0835c595460fae8546f950f17a1725a
Harry MacCode <emarh.harry.code@gmail.com>
2024-06-15 13:44:48 +0200

    Mongoose ID Error

--- backend/middlewares/error.middleware.js
+++ backend/middlewares/error.middleware.js
@@ -1,8 +1,16 @@
+import ErrorHandler from "../utils/errorHandler.util.js";
+
 export default (err, req, res, next) => {
     let error = {
         statusCode: err?.statusCode || 500,
         message: err?.message || 'Internal Server Error'
     };
+
+    // Handle Invalid Mongoose ID Error
+    if (err.name === 'CastError') {
+        const message = `Resource not found. Invalid : ${err?.path}`;
+        error = new ErrorHandler(message, 404);
+    }
 
     if (process.env.NODE_ENV === 'DEVELOPMENT') {
         res.status(error.statusCode).json({
