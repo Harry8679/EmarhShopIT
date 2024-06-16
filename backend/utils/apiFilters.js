dbec0c69f5a45ae05b1cba7df7dcc30b7d99a013
Harry MacCode <emarh.harry.code@gmail.com>
2024-06-16 22:58:58 +0200

    Filter Pagination

--- backend/utils/apiFilters.js
+++ backend/utils/apiFilters.js
@@ -32,6 +32,15 @@ class APIFilters {
 
         return this;
     }
+
+    pagination(resPerPage){
+        const currentPage = Number(this.queryStr.page) || 1;
+        const skip = resPerPage * (currentPage - 1);
+
+        this.query = this.query.limit(resPerPage).skip(skip);
+
+        return this;
+    }
 }
 
 export default APIFilters;
