fa4b9455efecf1c0d7e83248d87eeeab568c7128
Harry MacCode <emarh.harry.code@gmail.com>
2024-06-20 21:39:42 +0200

    Protected middleware

---
+++ backend/middlewares/auth.middleware.js
@@ -0,0 +1,34 @@
+const ErrorHandler = require('../utils/errorHandler.util');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user.model');
+
+const asyncHanlder = require('express-async-handler');
+
+const protected = asyncHanlder(async (req, res, next) => {
+    try {
+        const token = req.cookies.token;
+        if (!token) {
+            res.status(401);
+            throw new ErrorHandler('Not authorized, please login');
+        }
+
+        // Verify token
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        // Get user id from token
+        const user = await User.findById(verified.id).select('-password');
+        if (!user) {
+            res.status(404);
+            throw new ErrorHandler('User not found.')
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(401);
+        throw new ErrorHandler('Not Authorized, please login');
+    }
+});
+
+
+
+module.exports = { protected };
