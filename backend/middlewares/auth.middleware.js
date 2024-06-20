8631893919310d58b84d61e19a62e02e341f0243
Harry MacCode <emarh.harry.code@gmail.com>
2024-06-20 21:42:07 +0200

    authorizeRoles middleware

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -29,6 +29,14 @@ const protected = asyncHanlder(async (req, res, next) => {
     }
 });
 
+const authorizeRoles = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return next(new ErrorHandler(`Role ${req.user.role} is not allowed to access this resource.`, 403));
+        }
 
+        next();
+    };
+};
 
-module.exports = { protected };
+module.exports = { protected, authorizeRoles };
