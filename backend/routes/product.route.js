7a065134dc2e6e33980695e5af65608a69c02f79
Harry MacCode <emarh.harry.code@gmail.com>
2024-06-22 07:08:53 +0200

    Authorize User Roles and Permissions

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { deleteProduct, getAllProducts, getProductDetails, newProduct, updateProduct } from '../controllers/product.controller.js';
-import { isAuthenticated } from '../middlewares/auth.middleware.js';
+import { authorizeRoles, isAuthenticated } from '../middlewares/auth.middleware.js';
 
 const productsRoute = express.Router();
 
-productsRoute.get('/', isAuthenticated, getAllProducts);
+productsRoute.get('/', isAuthenticated, authorizeRoles('admin'), getAllProducts);
 productsRoute.post('/admin', newProduct);
 productsRoute.get('/:id', getProductDetails);
 productsRoute.put('/:id', updateProduct);
