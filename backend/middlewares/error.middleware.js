b9ddf09aaf8dd9a7a9ceaed667263a1a82fbc64d
Harry MacCode <emarh.harry.code@gmail.com>
2024-07-13 23:19:33 +0200

    Handle Wrong JWT Error

--- backend/middlewares/error.middleware.js
+++ backend/middlewares/error.middleware.js
@@ -21,8 +21,13 @@ export default (err, req, res, next) => {
     // Handle Mongoose Duplicate Key Error
     if (err.code === 11000) {
         const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
-        error = new ErrorHandler(message, 404);
+        error = new ErrorHandler(message, 400);
     }
+
+    // Handle Wrong JWT Error
+    if (err.name === 'JsonWebTokenError') {
+        const message = 'Json Web Token is oinvalid. Try again !';
+        error = new ErrorHandler(message, 400);
 
     if (process.env.NODE_ENV === 'DEVELOPMENT') {
         res.status(error.statusCode).json({
