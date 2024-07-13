25602c9c045732c61498e4a4966495a955e66d8b
Harry MacCode <emarh.harry.code@gmail.com>
2024-07-13 23:40:09 +0200

    Get Current User Profile Route

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { forgotPassword, loginUser, logout, register, resetPassword } from '../controllers/auth.controller.js';
+import { forgotPassword, getUserProfile, loginUser, logout, register, resetPassword } from '../controllers/auth.controller.js';
 
 const authRoute = express.Router();
 
@@ -8,6 +8,5 @@ authRoute.post('/login', loginUser);
 authRoute.get('/logout', logout);
 authRoute.post('/password/forgot', forgotPassword);
 authRoute.put('/password/reset/:token', resetPassword);
-// authRoute.put('/password/reset/:token', resetPassword);
-
+authRoute.get('/me', getUserProfile);
 export default authRoute;
