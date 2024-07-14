314dbe339928158e449722ea18aad1500e3c80b0
Harry MacCode <emarh.harry.code@gmail.com>
2024-07-14 09:04:01 +0200

    Update Password Route

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { forgotPassword, getUserProfile, loginUser, logout, register, resetPassword } from '../controllers/auth.controller.js';
+import { forgotPassword, getUserProfile, loginUser, logout, register, resetPassword, updatePassword } from '../controllers/auth.controller.js';
 import { isAuthenticated } from '../middlewares/auth.middleware.js';
 
 const authRoute = express.Router();
@@ -10,4 +10,5 @@ authRoute.get('/logout', logout);
 authRoute.post('/password/forgot', forgotPassword);
 authRoute.put('/password/reset/:token', resetPassword);
 authRoute.get('/me', isAuthenticated, getUserProfile);
+authRoute.post('/password/update', isAuthenticated, updatePassword);
 export default authRoute;
