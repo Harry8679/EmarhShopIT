b78d8fb242486033fc14b8d27ef0af6bb73d9451
Harry MacCode <emarh.harry.code@gmail.com>
2024-07-16 22:58:46 +0200

    Get All Orders Route

--- backend/routes/order.route.js
+++ backend/routes/order.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { isAuthenticated } from '../middlewares/auth.middleware.js';
-import { getOrderDetails, myOrders, newOrder } from '../controllers/order.controller.js';
+import { authorizeRoles, isAuthenticated } from '../middlewares/auth.middleware.js';
+import { getAllOrders, getOrderDetails, myOrders, newOrder } from '../controllers/order.controller.js';
 const orderRoute = express.Router();
 
 orderRoute.post('/new', isAuthenticated, newOrder);
 orderRoute.get('/me', isAuthenticated, myOrders);
+orderRoute.get('/all', isAuthenticated, authorizeRoles('admin'), getAllOrders);
 orderRoute.get('/:id', isAuthenticated, getOrderDetails);
 
 export default orderRoute;
